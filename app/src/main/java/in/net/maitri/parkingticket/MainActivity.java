dba12141c4bb1bbf59bd950835ab1d561c62fb4a
vishnuruban <vishnuruban93@gmail.com>
2018-01-19 09:42:36 +0530

    Initial commit

---
+++ app/src/main/java/in/net/maitri/parkingticket/MainActivity.java
@@ -0,0 +1,13 @@
+package in.net.maitri.parkingticket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
